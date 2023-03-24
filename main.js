57cec28d4ed98abbb1b5ef3e477bde821bfbe139
ricardo lg <solop162@gmail.com>
2023-03-23 21:48:41 -0300

    new commit

---
+++ main.js
@@ -0,0 +1,91 @@
+// Promises 
+// .then / .catch para ouvi os resultados
+
+//resolve funcao para indica que deu tudo certo
+//rejec e uma funcao para que deu errado
+
+// 1 Ex
+const somadoisnumeros = (a , b) =>{
+    return  new Promise((resolve, reject)=> {
+        setTimeout(() => {
+            resolve( a < b ? a + b : a - b);
+            reject();
+        }, 2000);
+    });
+}
+
+somadoisnumeros(1, 3)
+    .then(soma =>{
+        document.body.innerText = soma;
+    })
+
+// para tratar o erro da promise
+    .catch(err => {
+        console.log(err)
+    })
+
+
+
+// 2 Ex
+fetch('https://api.github.com/users/ricardolg')
+
+// Melhor
+    .then(response => {
+        return response.json();
+    })
+    .then(body => {
+        console.log(body)
+    })
+/*
+    .then(reponse => {
+        reponse.json().then(body => {
+            console.log(body);
+        })
+    })
+*/
+    .catch(err => {
+        console.log(err)
+    })
+
+    .finally(() => {
+        console.log("Final")
+    })
+
+   async function buscaDadosNogit(){
+    try {
+
+    const response = await fetch('https://api.github.com/users/ricardolg');
+    const body = await response.json();
+
+    //console.log(body);
+    return body.login
+}
+    catch (err) {
+        console.log(err);
+    }
+    finally {
+        console.log('fim');
+    }
+    /*
+    fetch('https://api.github.com/users/ricardolg')
+
+    .then(response => {
+        return response.json();
+    })
+    .then(body => {
+        console.log(body)
+    })
+
+    .catch(err => {
+        console.log(err)
+    })
+
+    .finally(() => {
+        console.log("Final")
+    })
+    */
+    }
+
+    buscaDadosNogit().then(login=> {
+        console.log(login)
+    });
